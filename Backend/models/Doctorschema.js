5639c796e26369036e440619e3a803ed59cbf607
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2024-07-30 23:07:06 -0900

    Schema

--- Backend/models/Doctorschema.js
+++ Backend/models/Doctorschema.js
@@ -1,42 +1,44 @@
 import mongoose from "mongoose";
 
-const Doctorschema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-    name: { type: String, required: true},
-    phone: { type: Number},
-    photo: { type: String},
-    ticketprice: { type: Number},
-    role: {
-        type: String, 
-    },
+const DoctorSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  phone: { type: Number },
+  photo: { type: String },
+  ticketPrice: { type: Number },
+  role: {
+    type: String,
+  },
 
-    //Doctors Field
-    specialization: { type: String},
-    qualification: {
-        type: Array,
-    },
-    experience: {
-        type: Array,
-    },
-    bio: {type: String, maxLength: 50},
-    about: {type: String},
-    timeslots: { type: Array},
-    reviews: [{ type: mongoose.Types.ObjectId, ref: "Review"}],
-    averageRating: {
-        type: Number,
-        default: 0,
-    },
-    totalRating: {
-        type: Number,
-        default: 0,
-    },
-    isApproved: {
-        type: String,
-        enum: ["pending", "approved", "cancelation"],
-        default: "pending",
-    },
-    appointments: [{ type: mongoose.Types.ObjectId, ref: "Appointment"}],
+  // Fields for doctors only
+  specialization: { type: String },
+  qualifications: {
+    type: Array,
+  },
+
+  experiences: {
+    type: Array,
+  },
+
+  bio: { type: String, maxLength: 50 },
+  about: { type: String },
+  timeSlots: { type: Array },
+  reviews: [{ type: mongoose.Types.ObjectId, ref: "Review" }],
+  averageRating: {
+    type: Number,
+    default: 0,
+  },
+  totalRating: {
+    type: Number,
+    default: 0,
+  },
+  isApproved: {
+    type: String,
+    enum: ["pending", "approved", "cancelled"],
+    default: "pending",
+  },
+  appointments: [{ type: mongoose.Types.ObjectId, ref: "Appointment" }],
 });
 
 export default mongoose.model("Doctor", DoctorSchema);
