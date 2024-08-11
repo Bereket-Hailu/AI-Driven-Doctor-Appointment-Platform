277f1fce5e23acbad73c24080b983d5714732c0d
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2024-08-11 13:09:43 -0900

    Routes

---
+++ Backend/Routes/user.js
@@ -0,0 +1,13 @@
+
+import express from "express"
+import {updateUser, deleteUser, getSingleUser, getAllUser} from "../controllers/userController.js"
+import { authenticate } from "../auth/verifyToken.js";
+
+const router = express.Router();
+
+router.get("/:id",authenticate, getSingleUser);
+router.get("/", getAllUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+export default router;
