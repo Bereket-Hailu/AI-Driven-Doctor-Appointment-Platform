0bb8a3b4baaa09d072c86c75c84bcac9bdab4fb5
nardoki <mihrethabtamu1902@gmail.com>
2024-08-14 23:48:43 +0300

    frontend update

---
+++ src/assets/data/services.js
@@ -0,0 +1,38 @@
+export const services = [
+  {
+    name: "Neurology",
+    desc: "Neurology focuses on the diagnosis and treatment of disorders related to the nervous system, which includes the brain, spinal cord, and peripheral nerves.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Heart and vascular",
+    desc: "Heart and vascular health refers to the study and treatment of conditions affecting the heart (cardiovascular system) and blood vessels (vascular system). ",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+  {
+    name: "ophthalmologist",
+    desc: "An ophthalmologist specializes in the diagnosis, treatment, and management of eye disorders and vision problems. cardio is the best exercise ever happen in my life ",//don't forget
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Endocrinologist",
+    desc: "An endocrinologist specializes in the diagnosis and treatment of disorders related to the endocrine system, which is responsible for hormone production and regulation in the body.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Cancer care",
+    desc: "Cancer care encompasses a comprehensive approach to the diagnosis, treatment, and support of individuals diagnosed with cancer. ",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Skin and hair treatment",
+    desc: "Skin and hair treatment involves a variety of medical and cosmetic approaches to address conditions affecting the skin and hair.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+];
